c408d1153aeaee340ef0732ea31b1e24a11d9c8c
Eugene Smyrnov <helldaf@yandex.ru>
2017-03-12 23:31:36 +0200

    add some

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+        int[] array = bubbleSortFlag(createArray(10));
+        for (int i : array)
+            System.out.println(i);
 
     }
 
@@ -12,9 +15,15 @@ public class Main {
         return array;
     }
 
+    //Custom sort
+    public static int[] sort(int[] array) {
+
+        return array;
+    }
+
     //List of implementation of different sorting algorithms:
     // 1. Insertion Sort
-    // Complexity: B Ω(n)	A Θ(n^2)	W O(n^2)	M O(1)
+    // Complexity: B Ω(n)	A Θ(n^2)	W O(n^2)	M O(1)    Stable
     // Pros:
     //  Efficient for sorting of small data
     //  Efficient for data that are almost sorted
@@ -38,24 +47,71 @@ public class Main {
         return array;
     }
 
-    //Selection Sort
-    //Complexity: B Ω(n^2)	A Θ(n^2)	W O(n^2)	M O(1)
+    //2. Selection Sort
+    //Complexity: B Ω(n^2)	A Θ(n^2)	W O(n^2)	M O(1) Unstable
     //Pros
     // Easy to implement 
     //Cons
     //  Is no faster on a partially sorted array
-    public static int[] selectionSort(int[] array){
+
+    static int[] selectionSort( int[] array) {
 
         for (int i = 0; i < array.length - 1; i++) {
             int index = i;
-            for (int j = i + 1; j < array.length; j++)
-                if (array[j] < array[index])
+            for (int j = i + 1; j < array.length; j++) {
+                if ( array[j] < array[index]) {
                     index = j;
-
-            int smallerNumber = array[index];
+                }
+            }
+            int temp = array[index];
             array[index] = array[i];
-            array[i] = smallerNumber;
+            array[i] = temp;
         }
         return array;
     }
+
+    //3. Bubble Sort
+    //Complexity: B Ω(n)	A Θ(n^2)	W O(n^2)	M O(1) Stable
+    //
+    //
+    //
+    //
+    //
+
+    static int[] bubbleSort(int[] array) {
+        int i;
+        int j;
+        int temp;
+
+        for (i = 0; i < array.length - 1; i++) {
+            for (j = 0; j < array.length - 1 - i; j++) {
+                if (array[j] > array[j + 1]) {
+                    temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+        return array;
+    }
+
+    static int[] bubbleSortFlag(int[] array) {
+        boolean swapped = true;
+        int j = 0;
+        int tmp;
+        while (swapped) {
+            swapped = false;
+            j++;
+            for (int i = 0; i < array.length - j; i++) {
+                if (array[i] > array[i + 1]) {
+                    tmp = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = tmp;
+                    swapped = true;
+                }
+            }
+        }
+        return array;
+    }
+
 }
