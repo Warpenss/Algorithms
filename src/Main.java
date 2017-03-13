e6810cb916311a906a9c8f06daebe78cf290f5ae
Eugene Smyrnov AcerE5 <helldaf@yandex.ru>
2017-03-13 15:09:15 +0200

    quick

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        int[] array = bubbleSortFlag(createArray(10));
+        int[] array = createArray(10);
+        quickSort(array, 0, array.length - 1);
         for (int i : array)
             System.out.println(i);
-
     }
 
     //Method for creating an array
@@ -112,6 +112,45 @@ public class Main {
             }
         }
         return array;
+    }
+
+
+    //4. Quick Sort
+    //
+    //
+    //
+    //
+    //
+
+    static int partition(int arr[], int left, int right)
+    {
+        int i = left, j = right;
+        int tmp;
+        int pivot = arr[(left + right) / 2];
+
+        while (i <= j) {
+            while (arr[i] < pivot)
+                i++;
+            while (arr[j] > pivot)
+                j--;
+            if (i <= j) {
+                tmp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = tmp;
+                i++;
+                j--;
+            }
+        };
+
+        return i;
+    }
+
+    static void quickSort(int arr[], int left, int right) {
+        int index = partition(arr, left, right);
+        if (left < index - 1)
+            quickSort(arr, left, index - 1);
+        if (index < right)
+            quickSort(arr, index, right);
     }
 
 }
